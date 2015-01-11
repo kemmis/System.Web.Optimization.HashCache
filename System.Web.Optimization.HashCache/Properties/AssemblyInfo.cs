ac05acbb28f103d2f7ef402004dddf08c0f961b0
Rafe Kemmis <rafe@kemmis.info>
2015-01-11 14:46:23 -0500

    added nuget & src publishing

--- System.Web.Optimization.HashCache/Properties/AssemblyInfo.cs
+++ System.Web.Optimization.HashCache/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("System.Web.Optimization.HashCache")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A helper bundle transform which prevents caching of bundle contents when optimizations are disabled - when debug is enabled or BundleTable.EnableOptimizations is false.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Rafe Kemmis")]
 [assembly: AssemblyProduct("System.Web.Optimization.HashCache")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Rafe Kemmis 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
